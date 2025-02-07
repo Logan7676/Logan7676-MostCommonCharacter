963ef3d1e18ce75807b078f283620700b1eaaefb
Logan Peters <logandpeters@gmail.com>
2025-02-07 19:29:10 +0000

    Completed Most Common Character Coding Challenge

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,4 @@
-
+import java.util.*;   
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +8,33 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap <Character, Integer> map = new HashMap<>();
+        int count = 0;
+        int maxCount=0;
+        Character c = str.charAt(0);
+
+        for(int i = 0; i < str.length(); i++) {
+            if (!map.containsKey(str.charAt(i))) {
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        count++;
+                    }
+                }
+                map.put(str.charAt(i), count);
+                count = 0;
+            }
+        }
+
+        for (Map.Entry<Character, Integer> mp:map.entrySet()) {
+            if (mp.getValue() > maxCount) {
+                maxCount = mp.getValue();
+            }
+        }
+        for (Map.Entry<Character, Integer> mp: map.entrySet()) {
+            if (maxCount == mp.getValue()) {
+                c = mp.getKey();
+            }
+        }
+        return c;
     }
 }
